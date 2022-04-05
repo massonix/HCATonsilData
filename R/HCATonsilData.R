aeaba8a5e13a82bc7e2ffa358befd9b924d4c57f
Ramon Massoni-Badosa <ramon.massoni@cnag.crg.eu>
2022-04-05 13:53:59 +0200

    can create SingleCellExperiment

--- R/HCATonsilData.R
+++ R/HCATonsilData.R
@@ -20,6 +20,7 @@
 #'
 #' @examples
 HCATonsilData <- function(assayType, cellType, processedCounts = TRUE) {
+  # Sanity checks
   allowedAssays <- c("RNA", "ATAC", "CITE", "Spatial")
   if (!(assayType %in% allowedAssays)) {
     stop(
@@ -32,6 +33,50 @@ HCATonsilData <- function(assayType, cellType, processedCounts = TRUE) {
       "'assay_type' must be included in ",
       paste(allowedTypes, collapse = ", "))
   }
+
+  # Initialize ExperimentHub object
   eh <- ExperimentHub::ExperimentHub()
   host <- file.path("HCATonsilData", "1.0", assayType)
+
+  # Define paths
+  suffixes <- c("counts.h5", "processed.h5", "rowdata.rds", "coldata.rds",
+                "pca.rds", "harmony.rds", "umap.rds")
+  names(suffixes) <- c("counts", "processedCounts", "rowData", "colData",
+                       "pca", "harmony", "umap")
+  filePaths <- sapply(suffixes, \(.) {
+    x <- file.path(host, paste(cellType, assayType, ., sep = "_"))
+    x
+  })
+  for (x in filePaths) {
+    if (sum(x == eh$rdatapath) > 1) {
+      stop("Input matched more than one entry!")
+    }
+  }
+
+  # Create SingleCellExperiment
+  cts <- HDF5Array::HDF5Array(
+    eh[eh$rdatapath == filePaths["counts"]][[1]],
+    name = "counts"
+  )
+  sce <- SingleCellExperiment::SingleCellExperiment(
+    assays = list(counts = cts),
+    rowData = eh[eh$rdatapath == filePaths["rowData"]][[1]],
+    colData = eh[eh$rdatapath == filePaths["colData"]][[1]]
+  )
+  SingleCellExperiment::reducedDims(sce) <- list(
+    PCA = eh[eh$rdatapath == filePaths["pca"]][[1]],
+    UMAP = eh[eh$rdatapath == filePaths["umap"]][[1]]
+  )
+  if (sum(filePaths["harmony"] == eh$rdatapath) == 1) {
+    harmony <- eh[eh$rdatapath == filePaths["harmony"]][[1]]
+    SingleCellExperiment::reducedDims(sce)[["HARMONY"]] <- harmony
+  }
+  if (processedCounts) {
+    prccts <- HDF5Array::HDF5Array(
+      eh[eh$rdatapath == filePaths["counts"]][[1]],
+      name = "processed"
+  )
+  SummarizedExperiment::assay(sce, "logcounts", withDimnames = FALSE) <- prccts
+  }
+  sce
 }
