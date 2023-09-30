eafb6ba46e4ae23341f14d5623f82ed3a370dc2e
Ramon Massoni-Badosa <ramon.massoni@cnag.crg.eu>
2023-09-30 20:27:05 +0200

    init make spatial metadata

---
+++ inst/scripts/make-metadata-tonsil-atlas-spatial_v2.R
@@ -0,0 +1,114 @@
+# This script generates the metadata.csv file for the Spatial data as requested in the following
+# tutorial: https://bioconductor.org/packages/devel/bioc/vignettes/HubPub/inst/doc/CreateAHubPackage.html
+# Note that this script is inspired in https://github.com/fmicompbio/TabulaMurisSenisData/blob/master/inst/scripts/make-metadata-tabula-muris-senis-droplet.R
+
+# Load packages
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(here)
+library(glue)
+library(purrr)
+
+
+# Get data slots
+data_dir <- here("inst/scripts/HCATonsilData/2.0/Spatial")
+slots <- list.files(data_dir)
+
+
+# Generate metadata file for ExperimentHub package
+slots_no_image <- slots %>%
+  str_subset("(image|scale)", negate = TRUE)
+slots_names <- slots_no_image %>%
+  str_split("_") %>%
+  map_chr(\(x) str_subset(x, "(.rds|.h5)")) %>%
+  str_remove("(.rds|.h5)")
+titles <- c(counts = "counts", logcounts = "processed counts", coldata = "colData",
+            rowdata = "rowData", pca = "PCA", harmony = "HARMONY", umap = "UMAP")
+descs <- c(counts = "Count matrix", logcounts = "Processed count matrix", coldata = "Cell metadata",
+           rowdata = "Gene annotation", pca = "PCA representation", harmony = "HARMONY representation",
+           umap = "UMAP representation")
+rdclass <- c(counts = "H5File", logcounts = "H5File", coldata = "DFrame",
+             rowdata = "DFrame", pca = "matrix", harmony = "matrix",
+             umap = "matrix")
+names(slots_no_image) <- slots_names
+metadata_df <- data.frame(
+  Title = glue("Tonsil Atlas Visium data {titles[slots_names]}"),
+  Description = glue("{descs[slots_names]} for the Tonsil Cell Atlas Visium dataset"),
+  RDataPath = glue("HCATonsilData/2.0/Spatial/{slots_no_image[slots_names]}"),
+  BiocVersion = "3.18",
+  Genome = "GRCh38",
+  SourceType = "HDF5",
+  SourceUrl = "https://zenodo.org/record/8373756",
+  SourceVersion = "2.0",
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  Coordinate_1_based = NA,
+  DataProvider = "BCLL@las",
+  Maintainer = "Ramon Massoni-Badosa <ramonmassoni@gmail.com>",
+  RDataClass = rdclass[slots_names],
+  DispatchClass = ifelse(rdclass[slots_names] == "H5File", "H5File", "Rds")
+)
+
+
+#
+    RDataPath = glue("HCATonsilData/2.0/RNA/{cell_type}_{dataset}_{outs}{suffix}"),
+)
+
+
+
+
+convs <- c(coldata = "colData", counts = "counts", processed = "processed counts",
+           rowdata = "rowData", pca = "PCA", harmony = "HARMONY", umap = "UMAP")
+convs2 <- c(coldata = "Cell metadata", counts = "Count matrix",
+            processed = "Processed count matrix", rowdata = "Gene annotation",
+            pca = "PCA representation", harmony = "HARMONY representation",
+            umap = "UMAP representation")
+suffix <- c(coldata = ".rds", counts = ".h5", processed = ".h5", rowdata = ".rds",
+            pca = ".rds", harmony = ".rds", umap = ".rds")
+rdclass <- c(coldata = "DFrame", counts = "H5File", processed = "H5File",
+             rowdata = "DFrame", pca = "matrix", harmony = "matrix", umap = "matrix")
+out_df <- files_df %>%
+  mutate(outs = "coldata;counts;processed;rowdata;pca;harmony;umap") %>%
+  separate_rows(outs, sep = ";") %>%
+  mutate(
+    outs2 = convs[outs],
+    descs = convs2[outs],
+    suffix = suffix[outs]
+  ) %>%
+  mutate(cell_type = str_replace(cell_type, "(/| )", "_")) %>%
+  mutate(
+    Title = glue("Tonsil Atlas RNA {cell_type} {outs2}"),
+    Description = glue("{descs} for the Tonsil Cell Atlas {cell_type} scRNA-seq/Multiome dataset"),
+    RDataPath = glue("HCATonsilData/2.0/RNA/{cell_type}_{dataset}_{outs}{suffix}"),
+    BiocVersion = "3.18",
+    Genome = "GRCh38",
+    SourceType = "HDF5",
+    SourceUrl = "https://zenodo.org/record/8373756",
+    SourceVersion = "2.0",
+    Species = "Homo sapiens",
+    TaxonomyId = "9606",
+    Coordinate_1_based = NA,
+    DataProvider = "BCLL@las",
+    Maintainer = "Ramon Massoni-Badosa <ramonmassoni@gmail.com>",
+    RDataClass = rdclass[outs],
+    DispatchClass = ifelse(suffix == ".h5", "H5File", "Rds")
+  ) %>%
+  select(Title, Description, RDataPath, BiocVersion, Genome,
+         SourceType, SourceUrl, SourceVersion, Species, TaxonomyId,
+         Coordinate_1_based, DataProvider, Maintainer, RDataClass,
+         DispatchClass)
+
+
+# Check that all files exist
+files <- file.path("HCATonsilData", list.files(path_to_data, recursive = TRUE))
+out_df <- out_df[out_df$RDataPath %in% files, ]
+
+
+# Write
+write_delim(
+  out_df,
+  file = here("inst/extdata/metadata-tonsil-atlas-rna.csv"),
+  delim = ","
+)
